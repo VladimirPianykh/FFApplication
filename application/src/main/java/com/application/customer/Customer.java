7a23d826f6d441c0e7b2fd7fe4be3142bfd4fb4c
boris <borisaushev.git@gmail.com>
2024-11-16 10:16:07 +0300

    Переделал классы с информацией

--- application/src/main/java/com/application/customer/Customer.java
+++ application/src/main/java/com/application/customer/Customer.java
@@ -2,8 +2,9 @@ package com.application.customer;
 
 import com.futurefactory.Data;
 
-public class Customer extends Data.Editable{
+public class Customer extends Data.Editable {
 	//Название компании
-	public Customer(String name){super(name);}
-	// this.records.add(new ActionRecord(":EDITED", User.getActiveUser()));
+	public Customer(String name) {
+		super(name);
+	}
 }
