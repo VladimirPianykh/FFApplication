870c118d9e401a97bdd82fa31db8940b31783aa4
boris aushev <borisaushev.git@gmail.com>
2024-11-17 17:01:28 +0300

    Добавил форму заказа

--- application/src/main/java/com/application/editor/Editor.java
+++ application/src/main/java/com/application/editor/Editor.java
@@ -173,7 +173,11 @@ public class Editor extends JDialog implements IEditor{
 			tab2Name.setFont(new Font(Font.DIALOG,Font.BOLD,getHeight()/40));
 			tab2Name.setHorizontalAlignment(JLabel.CENTER);
 			tab2.add(tab2Name);
-			new OrderEditor((Order)editable,mainPanel).add(nameField);
+			//я так понял надо добавлять на tab2
+			//было: new OrderEditor((Order)editable,mainPanel).add(nameField);
+			//TODO: поправить если надо
+			new OrderEditor((Order)editable,tab2).add(nameField);
+
 			mainPanel.add(tab2,"tab2");
 		}else if(editable instanceof Customer){
 			new CustomerEditor((Customer)editable,mainPanel).add(nameField);
