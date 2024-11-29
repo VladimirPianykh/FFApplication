69ef19589944c3cefeaa296e00a56a4fe17a70f1
VladimirPianykh <vovapianykh21@gmail.com>
2024-11-29 07:45:23 +0400

    Refactored code, painted ShiftTaskBoard icon

--- application/src/main/java/com/application/ShiftTaskBoard.java
+++ application/src/main/java/com/application/ShiftTaskBoard.java
@@ -4,8 +4,6 @@ import java.awt.BasicStroke;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-
 import javax.swing.JPanel;
 
 import com.futurefactory.User.Feature;
@@ -23,11 +21,10 @@ public class ShiftTaskBoard implements Feature{
 	}
 	@Override
 	public void paint(Graphics2D g2,BufferedImage image,int s){
-
+		g2.setStroke(new BasicStroke(s/50));
 		g2.drawLine(s/4,s/3,s*3/4,s/3);
 		g2.drawLine(s/4,s/2,s*3/4,s/2);
 		g2.drawLine(s/4,s*2/3,s*3/4,s*2/3);
-		throw new UnsupportedOperationException("Unimplemented method 'paint'");
     }
 
 }
